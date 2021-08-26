0c1da078ae821c76fbe8a3d36bef552ef7088716
edewell <edewell3@gmail.com>
2021-08-25 23:01:32 -0500

    index.html

---
+++ main.js
@@ -0,0 +1,45 @@
+"use strict"
+
+let askName = prompt("Can I get your name?");  
+
+ console.log(askName);  
+
+ alert("Hey" + askName)
+
+ 
+ 
+ let response = prompt("Was 102 easy for you?") 
+
+ if (response.toLowerCase() === 'y' || response.toLowerCase() === 'yes') 
+
+    { 
+     alert("awesome") 
+    } 
+
+    
+let plan = prompt("Do you plan to keep going?") 
+
+    if (response.toLowerCase() === 'y' || response.toLowerCase() === 'yes') 
+   
+       { 
+        alert("awesome") 
+       } 
+
+let confidence = prompt("Are you confident?") 
+
+ if (response.toLowerCase() === 'y' || response.toLowerCase() === 'yes') 
+
+    { 
+     alert("awesome") 
+    } 
+
+
+    let number = prompt('On a scaleof 1-10, How confident are you?')
+    {
+    
+    
+        if (response  > 11)
+        alert("Too high")
+    
+    }
+    
